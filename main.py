e00cd29a8b0429071956b50caa1b34228e1da3f9
Sathish Ramachandran <sathish.r26@gmail.com>
2021-02-05 01:57:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+x = int(input("Can you please enter a number? "))
+print("Increasing it by 4: ") 
+x += 4
+print(x)
+print("Decreasing it by 4: ")
+x -= 4
+print(x)
+print("Multiplying it by 2: ")
+x *= 2
+print(x)
+print("Dividing it by 3: ")
+x /= 3
+print(x)
